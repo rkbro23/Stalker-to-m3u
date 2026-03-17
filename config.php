fd53591f44d1e59e1084562efbe763b3fa95fafc
RKM3U <ben929628@gmail.com>
2026-03-18 03:24:04 +0530

    Refactor Stalker Portal configuration and functions

--- config.php
+++ config.php
@@ -1,43 +1,48 @@
 <?php
-// Configuration file for Stalker Portal credentials
+// Configuration for main.light-ott.net
 $stalkerCredentials = [
-    'host' => 'main.light-ott.net',
-    'mac' => '00:1A:79:66:94:44'
+    'host'      => 'main.light-ott.net',
+    'mac'       => '00:1A:79:66:94:44',
+    'base_path' => '/c',               // from http://main.light-ott.net/c/
+    'api_file'  => 'portal.php',        // the API endpoint
+    'stb_type'  => 'MAG270'             // as seen in debug
 ];
 
 /**
- * Generate dynamic device hashes from MAC (same as Python script)
+ * Generate device hashes exactly like the scanner (Ultima_SpeedX.py)
  */
 function generateDeviceHashes($mac) {
     $mac_clean = strtoupper($mac);
     
-    // SN: first 13 chars of MD5 (uppercase)
-    $sn = strtoupper(substr(md5($mac_clean), 0, 13));
+    // Full MD5 of MAC (uppercase)
+    $sn_full = strtoupper(md5($mac_clean));
+    // SN cut: first 13 chars (what the portal expects)
+    $sn_cut = substr($sn_full, 0, 13);
     
     // Device ID 1: SHA256 of MAC
     $dev_id = strtoupper(hash('sha256', $mac_clean));
     
-    // Device ID 2: SHA256 of MAC + 'mag250'
-    $dev_id2 = strtoupper(hash('sha256', $mac_clean . 'mag250'));
+    // Device ID 2: SHA256 of sn_cut
+    $dev_id2 = strtoupper(hash('sha256', $sn_cut));
     
-    // Signature: SHA256 of MAC + 'signature'
-    $signature = strtoupper(hash('sha256', $mac_clean . 'signature'));
+    // Signature: SHA256 of sn_cut . mac
+    $signature = strtoupper(hash('sha256', $sn_cut . $mac_clean));
     
     return [
-        'sn' => $sn,
-        'dev_id' => $dev_id,
-        'dev_id2' => $dev_id2,
+        'sn_cut'    => $sn_cut,
+        'dev_id'    => $dev_id,
+        'dev_id2'   => $dev_id2,
         'signature' => $signature
     ];
 }
 
 /**
- * Build headers with device spoofing (MAG250)
+ * Build headers with device spoofing (MAG270)
  */
 function generateDeviceHeaders($mac, $token = '') {
     $headers = [
-        "User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG250 stbapp ver: 4 rev: 250 Safari/533.3",
-        "X-User-Agent: Model: MAG250; Link: Ethernet",
+        "User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG270 stbapp ver: 2 rev: 250 Safari/533.3",
+        "X-User-Agent: Model: MAG270; Link: Ethernet",
         "Cookie: mac={$mac}; stb_lang=en; timezone=GMT",
         "Accept: application/json, application/javascript, text/javascript, text/html",
         "Accept-Encoding: gzip, deflate",
@@ -61,39 +66,37 @@ function handshake($host, $mac, $forceRegenerate = false) {
         return $tokenCache;
     }
     
+    global $stalkerCredentials;
     $hashes = generateDeviceHashes($mac);
-    $sn = $hashes['sn'];
-    $dev_id = $hashes['dev_id'];
-    $dev_id2 = $hashes['dev_id2'];
-    $signature = $hashes['signature'];
     
     $timestamp = time();
     $random = rand(100000, 999999);
     
-    // Metrics JSON (same as Python)
+    // Metrics JSON (same as scanner)
     $metrics = json_encode([
-        'mac' => $mac,
-        'sn' => $sn,
-        'model' => 'MAG250',
-        'type' => 'STB',
-        'uid' => $dev_id,
+        'mac'    => $mac,
+        'sn'     => $hashes['sn_cut'],
+        'model'  => $stalkerCredentials['stb_type'],
+        'type'   => 'STB',
+        'uid'    => $hashes['dev_id'],
         'random' => $random
     ]);
     
-    $url = "http://{$host}/stalker_portal/server/load.php";
+    // Build API URL – now using /c/portal.php
+    $url = "http://{$host}{$stalkerCredentials['base_path']}/{$stalkerCredentials['api_file']}";
     $params = [
-        'type' => 'stb',
-        'action' => 'handshake',
-        'token' => '',
+        'type'      => 'stb',
+        'action'    => 'handshake',
+        'token'     => '',
         'JsHttpRequest' => '1-xml',
-        'sn' => $sn,
-        'stb_type' => 'MAG250',
+        'sn'        => $hashes['sn_cut'],
+        'stb_type'  => $stalkerCredentials['stb_type'],
         'client_type' => 'STB',
-        'device_id' => $dev_id,
-        'device_id2' => $dev_id2,
-        'signature' => $signature,
+        'device_id' => $hashes['dev_id'],
+        'device_id2'=> $hashes['dev_id2'],
+        'signature' => $hashes['signature'],
         'timestamp' => $timestamp,
-        'metrics' => $metrics
+        'metrics'   => $metrics
     ];
     
     $fullUrl = $url . '?' . http_build_query($params);
